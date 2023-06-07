16e7a718627c9c009c57795a1f5c886ab5d443b4
Joa1zin <jvmm676@gmail.com>
2023-06-07 00:39:41 -0300

    criado script para compras

---
+++ javascript/carrinhoCompras.js
@@ -0,0 +1,48 @@
+let produtos = []
+
+function adicionaCompra1 (){
+    var priceItem1 = 79.90;
+    produtos.push(priceItem1)
+    alert('Arranjo Girassol adicionado ao carrinho!')
+}
+
+function adicionaCompra2 (){
+    var priceItem2 = 49.90;
+    produtos.push(priceItem2)
+    alert('Rosas Brancas adicionado ao carrinho!')
+}
+
+function adicionaCompra3 (){
+    var priceItem3 = 109.90;
+    produtos.push(priceItem3)
+    alert('Rosas Azuis adicionado ao carrinho!')
+}
+
+function adicionaCompra4 (){
+    var priceItem4 = 29.90;
+    produtos.push(priceItem4)
+    alert('Margaridas adicionado ao carrinho!')
+}
+
+function adicionaCompra5 (){
+    var priceItem5 = 99.90;
+    produtos.push(priceItem5)
+    alert('Buquê Rosas Pink adicionado ao carrinho!')
+}
+
+function adicionaCompra6 (){
+    var priceItem6 = 139.90;
+    produtos.push(priceItem6)
+    alert('Arranjo Astromélias adicionado ao carrinho!')
+}
+
+function paginaCompras(){
+    var somaTotal = 0
+    for(var i = 0; i < produtos.length; i++){
+        soma += produtos[i]
+    }
+    
+    document.write(`
+    <div>${soma}</div>
+    `)
+}
